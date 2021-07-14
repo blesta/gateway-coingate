e7346ee6dbdb021e8402086a675adce0d02d78bb
Abdy Franco <yosoy@abdyfran.co>
2021-07-14 14:48:24 -0600

    Log curl errors

--- lib/CoinGate.php
+++ lib/CoinGate.php
@@ -1,16 +1,22 @@
 <?php
+
 namespace CoinGate;
+
+use Blesta\Core\Util\Common\Traits\Container;
 
 class CoinGate
 {
-    const VERSION           = '2.0.1';
+    // Load traits
+    use Container;
+
+    const VERSION = '2.0.1';
     const USER_AGENT_ORIGIN = 'CoinGate PHP Library';
 
-    public static $appID      = '';
-    public static $apiKey     = '';
-    public static $apiSecret  = '';
+    public static $appID = '';
+    public static $apiKey = '';
+    public static $apiSecret = '';
     public static $environment = 'live';
-    public static $userAgent  = '';
+    public static $userAgent = '';
 
     public static function config($authentication)
     {
@@ -48,13 +54,14 @@ class CoinGate
 
     public static function request($url, $method = 'POST', $params = [], $authentication = [])
     {
-        $appID      = isset($authentication['app_id']) ? $authentication['app_id'] : self::$appID;
-        $apiKey     = isset($authentication['api_key']) ? $authentication['api_key'] : self::$apiKey;
-        $apiSecret  = isset($authentication['api_secret']) ? $authentication['api_secret'] : self::$apiSecret;
+        // Initialize logger
+        $logger = self::getFromContainer('logger');
+
+        $appID = isset($authentication['app_id']) ? $authentication['app_id'] : self::$appID;
+        $apiKey = isset($authentication['api_key']) ? $authentication['api_key'] : self::$apiKey;
+        $apiSecret = isset($authentication['api_secret']) ? $authentication['api_secret'] : self::$apiSecret;
         $environment = isset($authentication['environment']) ? $authentication['environment'] : self::$environment;
-        $userAgent  = isset($authentication['user_agent'])
-            ? $authentication['user_agent']
-            : (isset(self::$userAgent) ? self::$userAgent : (self::USER_AGENT_ORIGIN . ' v' . self::VERSION));
+        $userAgent = isset($authentication['user_agent']) ? $authentication['user_agent'] : (isset(self::$userAgent) ? self::$userAgent : (self::USER_AGENT_ORIGIN . ' v' . self::VERSION));
 
         # Check if credentials was passed
         if (empty($appID) || empty($apiKey) || empty($apiSecret)) {
@@ -66,31 +73,25 @@ class CoinGate
 
         if (!in_array($environment, $environments)) {
             $availableEnvironments = join(', ', $environments);
-            \CoinGate\Exception::throwException(
-                400,
-                [
+            \CoinGate\Exception::throwException(400, [
                     'reason' => 'BadEnvironment',
                     'message' => 'Environment does not exist. Available environments: ' . $availableEnvironments
-                ]
-            );
+                ]);
         }
 
-        $url = ($environment === 'sandbox'
-                ? 'https://api-sandbox.coingate.com/v1'
-                : 'https://api.coingate.com/v1'
-            ) . $url;
-        $nonce     = (int)(microtime(true) * 1e6);
-        $message   = $nonce . $appID . $apiKey;
+        $url = ($environment === 'sandbox' ? 'https://api-sandbox.coingate.com/v1' : 'https://api.coingate.com/v1') . $url;
+        $nonce = (int) (microtime(true) * 1e6);
+        $message = $nonce . $appID . $apiKey;
         $signature = hash_hmac('sha256', $message, $apiSecret);
-        $headers   = [];
+        $headers = [];
         $headers[] = 'Access-Key: ' . $apiKey;
         $headers[] = 'Access-Nonce: ' . $nonce;
         $headers[] = 'Access-Signature: ' . $signature;
-        $curl      = curl_init();
+        $curl = curl_init();
 
         $curlOptions = [
             CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL            => $url
+            CURLOPT_URL => $url
         ];
 
         if ($method == 'POST') {
@@ -102,10 +103,22 @@ class CoinGate
         curl_setopt_array($curl, $curlOptions);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-        $response    = json_decode(curl_exec($curl), true);
+        if (Configure::get('Blesta.curl_verify_ssl')) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+        } else {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+
+        $response = curl_exec($curl);
+        $response = json_decode($response, true);
         $httpStatus = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if ($response == false) {
+            $logger->error(curl_error($curl));
+        }
 
         if ($httpStatus === 200) {
             return $response;
